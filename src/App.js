4cba3b7a70c666f5a65e4ca883abcb6d6505b9bb
sdharamk14 <95117849+sdharamk14@users.noreply.github.com>
2022-01-04 14:47:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Container, Grid, Item } from "@mui/material";
+// import "./styles.css";
+
+const App = () => {
+  return (
+    // <div className="App">
+    // <h1>sadf</h1>
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+    </Grid>
+    // </div>
+  );
+};
+export default App;
